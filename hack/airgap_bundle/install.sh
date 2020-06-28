79df9b9a737a90cda872eba3793cf7f135424132
dexhorthy <dexter.i.horthy@gmail.com>
2020-06-28 15:54:03 -0500

    adding the rest of the secrets back in

--- hack/airgap_bundle/install.sh
+++ hack/airgap_bundle/install.sh
@@ -12,31 +12,6 @@ KOTS_REGISTRY_USERNAME=$4
 KOTS_REGISTRY_PASSWORD=$5
 KOTS_REGISTRY_NAMESPACE=$6
 
-
-validate() {
-  if [ -z "${KOTS_REGISTRY_URL}" ]; then usage "missing registry URL"; exit 1; fi
-  if [ -z "${KOTS_NAMESPACE}" ]; then usage "missing namespace"; exit 1; fi
-  if [ -z "${KOTS_IMAGE_PULL_SECRET_NAME}" ]; then usage "missing image pull secret name"; exit 1; fi
-}
-
-log() {
-  echo
-  echo "=========="
-  echo $@
-  echo "=========="
-}
-
-contains() {
-    string="$1"
-    substring="$2"
-    if test "${string#*$substring}" != "$string"
-    then
-        return 0
-    else
-        return 1
-    fi
-}
-
 usage() {
   cat <<EOF
 error: $@
@@ -61,7 +36,109 @@ KOTS_REGISTRY_NAMESPACE -- optional, a slash-prefixed registry namespace, e.g. "
 EOF
 }
 
+log() {
+  echo
+  echo "=========="
+  echo $@
+  echo "=========="
+}
+
+contains() {
+    string="$1"
+    substring="$2"
+    if test "${string#*$substring}" != "$string"
+    then
+        return 0
+    else
+        return 1
+    fi
+}
+
+
+validate() {
+  if [ -z "${KOTS_REGISTRY_URL}" ]; then usage "missing registry URL"; exit 1; fi
+  if [ -z "${KOTS_NAMESPACE}" ]; then usage "missing namespace"; exit 1; fi
+  if [ -z "${KOTS_IMAGE_PULL_SECRET_NAME}" ]; then usage "missing image pull secret name"; exit 1; fi
+}
+
+
+preflight() {
+  log "Checking for prerequisites: namespace exists, with image pull secret, and registry push credentials are valid"
+  if ! kubectl get namespace ${KOTS_NAMESPACE} >/dev/null; then
+    echo "FAIL: namespace ${KOTS_NAMESPACE} not found, please provide the name of an existing namespace"
+    exit 1
+  fi
+  echo "SUCCESS: namespace \"${KOTS_NAMESPACE}\" exists"
+
+  if ! kubectl get secret -n ${KOTS_NAMESPACE} ${KOTS_IMAGE_PULL_SECRET_NAME} >/dev/null; then
+    echo "FAIL: namespace ${KOTS_NAMESPACE} not found, please provide the name of an existing namespace"
+    exit 1
+  fi
+  echo "SUCCESS: secret \"${KOTS_IMAGE_PULL_SECRET_NAME}\" exists"
+
+  # if there are creds, validate w/ docker login
+  if [ ! -z "${KOTS_REGISTRY_USERNAME}${KOTS_REGISTRY_PASSWORD}" ]; then
+    if ! docker login --username ${KOTS_REGISTRY_USERNAME} --password ${KOTS_REGISTRY_PASSWORD} ${KOTS_REGISTRY_URL}; then
+      echo "FAIL: registry creds for ${KOTS_REGISTRY_URL} failed 'docker login', please very credentials have 'docker push' access"
+      exit 1
+    fi
+    echo "SUCCESS: push credentials valid"
+  else
+    echo "SUCCESS: no push credentials provided, skipping docker login."
+  fi
+
+  if kubectl get deploy -n ${KOTS_NAMESPACE} kotsadm-api 2>&1 >/dev/null; then
+    printf "FAIL: Deployment kotsadm-api already found in namespace, please ensure namespace is empty before installing. "
+    printf "Because many objects like services and secrets will have been created by a previous deployment, you should "
+    printf "proceed to delete the entire namespace and recreate it before trying again\n"
+    echo
+    echo "    kubectl delete namespace ${KOTS_NAMESPACE}"
+    echo "    kubectl create namespace ${KOTS_NAMESPACE}"
+    echo "    kubectl -n ${KOTS_NAMESPACE} create secret docker-registry registry-creds --docker-server=... --docker-username=... --docker-password=... --docker-email=a@b.c"
+    echo
+
+    exit 1
+  fi
+  echo "SUCCESS: it appears that namespace \"${KOTS_NAMESPACE}\" does not contain any existing KOTS resources from a previous deploy"
+
+}
+
 make_kustomization() {
+  log "preparing kustomization"
+
+  AUTO_CREATE_CLUSTER_TOKEN=$(< /dev/urandom tr -dc A-Za-z0-9 | head -c16)
+
+  cat <<EOF >./yaml/cluster-token.yaml
+apiVersion: v1
+kind: Secret
+metadata:
+  name: kotsadm-cluster-token
+  labels:
+    kots.io/kotsadm: 'true'
+    velero.io/exclude-from-backup: 'true'
+stringData:
+  kotsadm-cluster-token: ${AUTO_CREATE_CLUSTER_TOKEN}
+EOF
+
+  cat <<EOF >./yaml/cluster-token-patch.yaml
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: kotsadm-api
+  namespace: appname
+spec:
+  template:
+    spec:
+      containers:
+        - name: kotsadm-api
+          env:
+            - name: AUTO_CREATE_CLUSTER_TOKEN
+              valueFrom:
+                secretKeyRef:
+                  key: kotsadm-cluster-token
+                  name: kotsadm-cluster-token
+EOF
+
   cat <<EOF >./yaml/regcred.json
 [
   { "op": "add",
@@ -86,98 +163,67 @@ kind: Kustomization
 namespace: ${KOTS_NAMESPACE}
 resources:
 - ./kotsadm.yaml
+- ./cluster-token.yaml
+patches:
+- ./cluster-token-patch.yaml
 patchesJson6902:
-#  - path: ./regcred.json
-#    target:
-#      group: apps
-#      version: v1
-#      kind: Deployment
-#      name: kotsadm
+  - path: ./regcred.json
+    target:
+      group: apps
+      version: v1
+      kind: Deployment
+      name: kotsadm
   - path: ./regcred.json
     target:
       group: apps
       version: v1
       kind: Deployment
       name: kotsadm-api
-#  - path: ./regcred.json
-#    target:
-#      group: apps
-#      version: v1
-#      kind: Deployment
-#      name: kotsadm-operator
-#  - path: ./regcred.json
-#    target:
-#      group: apps
-#      version: v1
-#      kind: StatefulSet
-#      name: kotsadm-minio
-#  - path: ./regcred-pod.json
-#    target:
-#      group: core
-#      version: v1
-#      kind: Pod
-#      name: kotsadm-migrations-*
+  - path: ./regcred.json
+    target:
+      group: apps
+      version: v1
+      kind: Deployment
+      name: kotsadm-operator
+  - path: ./regcred.json
+    target:
+      group: apps
+      version: v1
+      kind: StatefulSet
+      name: kotsadm-minio
+  - path: ./regcred-pod.json
+    target:
+      group: core
+      version: v1
+      kind: Pod
+      name: kotsadm-migrations-*
 images:
 EOF
 
-
-}
-
-preflight() {
-  log "Checking for prerequisites: namespace exists, with image pull secret, and registry push credentials are valid"
-  if ! kubectl get namespace ${KOTS_NAMESPACE} >/dev/null; then
-    echo "FAIL: namespace ${KOTS_NAMESPACE} not found, please provide the name of an existing namespace"
+  # postflight for yaml
+  if ! kubectl kustomize ./yaml >/dev/null; then
+    echo "ERROR: invalid yaml after generating kustomize base in ./yaml"
     exit 1
   fi
-  echo "SUCCESS: namespace \"${KOTS_NAMESPACE}\" exists"
-
-  if ! kubectl get secret -n ${KOTS_NAMESPACE} ${KOTS_IMAGE_PULL_SECRET_NAME} >/dev/null; then
-    echo "FAIL: namespace ${KOTS_NAMESPACE} not found, please provide the name of an existing namespace"
-    exit 1
-  fi
-  echo "SUCCESS: secret \"${KOTS_IMAGE_PULL_SECRET_NAME}\" exists"
-
-  if [ ! -z "${KOTS_REGISTRY_USERNAME}${KOTS_REGISTRY_PASSWORD}" ]; then
-    if ! docker login --username ${KOTS_REGISTRY_USERNAME} --password ${KOTS_REGISTRY_PASSWORD} ${KOTS_REGISTRY_URL}; then
-      echo "FAIL: registry creds for ${KOTS_REGISTRY_URL} failed 'docker login', please very credentials have 'docker push' access"
-      exit 1
-    fi
-    echo "SUCCESS: push credentials valid"
-  else
-    echo "SUCCESS: no push credentials provided, skipping docker login."
-  fi
-
-  if kubectl get deploy -n ${KOTS_NAMESPACE} kotsadm-api 2>&1 >/dev/null; then
-    printf "FAIL: Deployment kotsadm-api already found in namespace, please ensure namespace is empty before installing. "
-    printf "Because many objects like services and secrets will have been created by a previous deployment, you should "
-    printf "proceed to delete the entire namespace and recreate it before trying again\n"
-    echo
-    echo "    kubectl delete namespace ${KOTS_NAMESPACE}"
-    echo "    kubectl create namespace ${KOTS_NAMESPACE}"
-    echo "    kubectl -n ${KOTS_NAMESPACE} create secret docker-registry registry-creds --docker-server=... --docker-username=... --docker-password=... --docker-email=a@b.c"
-    echo
-
-    exit 1
-  fi
-  echo "SUCCESS: namespace \"${KOTS_NAMESPACE}\" does not contain any existing KOTS resources"
+  echo "SUCCESS: valid kustomization yaml created in ./yaml"
 
 }
 
 load_images() {
-  log "loading"
+  log "loading docker images"
   for image in `ls ./images/`; do
     docker load < ./images/${image}
   done
 }
 
 tag_and_push_images() {
-  log "tagging and pushing"
+  log "tagging and pushing to ${KOTS_REGSITRY_URL}${KOTS_REGISTRY_NAMESPACE}"
   for image in `cat yaml/kotsadm.yaml | grep 'image: ' | cut -d':' -f 2,3 | cut -d ' ' -f2 | sort | uniq `; do
     if contains "${image}" "postgres"; then
         imageSuffixWithTag=${image}
         rewritePrefix=""
     else
-        imageSuffixWithTag=`echo ${image} | cut -d'/' -f 2`
+        imageSuffixWithTag=`echo ${image} | cut -d'/' -f 2` # kotsadm-api:1.16.1
         rewritePrefix="kotsadm/"
     fi
 
@@ -185,11 +231,10 @@ tag_and_push_images() {
     docker tag ${image} ${newName}
     docker push ${newName}
 
-    echo adding kustomization snippet for ${image}
-    echo
     imageSuffixWithoutTag=`echo ${imageSuffixWithTag} | cut -d':' -f 1`
     imageTag=`echo ${imageSuffixWithTag} | cut -d':' -f 2`
 
+    echo
     echo adding kustomization snippet for ${image}: ${imageSuffixWithoutTag} ${imageTag}
     echo
 
@@ -198,6 +243,7 @@ tag_and_push_images() {
   newName: ${KOTS_REGISTRY_URL}${KOTS_REGISTRY_NAMESPACE}/${imageSuffixWithoutTag}
   newTag: "${imageTag}"
 EOF
+
   done
 }
 
